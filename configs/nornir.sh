5c9622a6cd3543da4eb544695d29b259b2343f6f
esanchezv73 <ernesto@di.unsa.edu.ar>
2026-01-19 20:12:19 -0300

    Create nornir.sh

---
+++ configs/nornir.sh
@@ -0,0 +1,12 @@
+apt install python3-dev build-essential libssl-dev libffi-dev -y
+sleep 3
+pip install nornir nornir-netmiko nornir-napalm nornir-utils
+sleep 3
+pip install pynetbox
+sleep 1
+pip install nornir-jinja2
+sleep 1
+pip install nornir-netbox
+sleep 1
+pip install nornir-scrapli
+sleep 1
