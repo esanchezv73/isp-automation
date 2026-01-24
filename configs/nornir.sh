2c4fbefb010a020ae0e08b3213b4d838c7299b73
esanchezv73 <ernesto@di.unsa.edu.ar>
2026-01-24 20:55:21 -0300

    Update nornir.sh

--- configs/nornir.sh
+++ configs/nornir.sh
@@ -1,4 +1,4 @@
-apt install python3-dev build-essential libssl-dev libffi-dev -y
+apt install curl python3-dev build-essential libssl-dev libffi-dev -y
 sleep 3
 pip install nornir nornir-netmiko nornir-napalm nornir-utils
 sleep 3
