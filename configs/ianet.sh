056e5931e32490b045121c2e238fb8113727c27d
esanchezv73 <ernesto@di.unsa.edu.ar>
2026-06-12 23:15:33 -0300

    Create ianet.sh

---
+++ configs/ianet.sh
@@ -0,0 +1,12 @@
+apt update
+sleep 1
+apt install curl python3-dev build-essential libffi-dev -y
+sleep 1
+apt install python3-psycopg2 -y
+sleep 1
+apt install postgresql-client -y
+sleep 1
+pip install xgboost scikit-learn
+sleep 1
+pip install matplotlib pandas seaborn
+sleep 1
